5f548eb77155fa9b168f19cdfbd300c6e12e7fac
Raouf Aissaoui <raouf@hotmail.com>
2021-02-06 12:48:44 +0000

    Created Reacte Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from "react-router-dom";
+import Index from "./Components/Index";
+import Navbar from './Components/Navbar';
+import Contact from './Components/Contact';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar/>
+        <Route exact path="/" component={Index}/>
+        <Route path="/contact" component={Contact}/>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App;
